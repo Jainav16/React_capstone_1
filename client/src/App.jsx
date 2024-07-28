afb105b5cd17dcc1d1324890b8536fc7f86ecff3
Jainav <the.jainav@gmail.com>
2024-07-28 15:37:41 +0530

    imported multimedia assetts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import {BrowserRouter, Routes, Route } from "react-router-dom";
+import NotFoundPage from './pages/NotFoundPage';
 
 import RegisterPage from './pages/RegisterPage';
 
@@ -9,6 +10,13 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<RegisterPage />} />
+        <Route path='/register' element={<RegisterPage />}></Route>
+        <Route path='/home' element={<RegisterPage />}></Route>
+        <Route path='/genres' element={<RegisterPage />}></Route>
+        <Route path='/carousel' element={<RegisterPage />}></Route>
+        <Route path='/dashboard' element={<RegisterPage />}></Route>
+        <Route path='/movies' element={<RegisterPage />}></Route>
+        <Route path='*' element={<NotFoundPage />}></Route>
       </Routes>
     </BrowserRouter>
   )
