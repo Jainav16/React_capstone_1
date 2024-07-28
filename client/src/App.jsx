3e01d0763e349c639ae5c73f27006c33b816e161
Jainav <the.jainav@gmail.com>
2024-07-28 15:15:34 +0530

    implemented routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,16 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import {BrowserRouter, Routes, Route } from "react-router-dom";
 
-import './App.css'
+import RegisterPage from './pages/RegisterPage';
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<RegisterPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
